c89f6d59fa29dce878236a6e59235795d5f61c12
Al3x <roat_alexandru@yahoo.com>
2021-06-09 22:19:47 +0300

    files

--- scripts/index.js
+++ scripts/index.js
@@ -5,15 +5,24 @@ const invisibil = document.querySelector('.cell__invisible');
 cell.addEventListener('click', e => {
     
     if(e.target.tagName === 'I') {
-        const parinte = e.target.parentElement;
-        let firstChild = parinte.children[0];
-        let secondChild = parinte.children[1];
-        firstChild.classList.add('disp__non');
-        secondChild.classList.remove('disp__non');
+        if(!visibil.classList.contains('disp__non')) {
+            const parinte = e.target.parentElement;
+            let firstChild = parinte.children[0];
+            let secondChild = parinte.children[1];
+
+            firstChild.classList.add('disp__non');
+            secondChild.classList.remove('disp__non');
         
         
-        let clasFirst = firstChild.className.split(' ');
-        console.log(firstChild, secondChild, clasFirst[1]);
+            let clasFirst = firstChild.className.split(' ');
+            console.log(firstChild, secondChild, clasFirst[1]);
+            console.log('are')
+
+        }else{
+            visibil.classList.remove('disp__non')
+            invisibil.classList.add('disp__non')
+            console.log('nu are, dar sar putea sa aiaba')
+        }
     }
 });
 
