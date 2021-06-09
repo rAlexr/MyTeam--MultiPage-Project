9dcccb5029f4cf51b3fda53e6adc365cc5d6c60f
Al3x <roat_alexandru@yahoo.com>
2021-06-09 16:36:47 +0300

    files

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,17 @@ const visibil = document.querySelector('.cell__visibile');
 const invisibil = document.querySelector('.cell__invisible');
 
 cell.addEventListener('click', e => {
+    
     if(e.target.tagName === 'I') {
         const parinte = e.target.parentElement;
-        visibil.style.display = 'none';
-        invisibil.classList.remove('disp__non');
-        console.log(parinte);
-    } 
+        let firstChild = parinte.children[0];
+        let secondChild = parinte.children[1];
+        firstChild.classList.add('disp__non');
+        secondChild.classList.remove('disp__non');
+        
+        
+        let clasFirst = firstChild.className.split(' ');
+        console.log(firstChild, secondChild, clasFirst[1]);
+    }
 });
+
