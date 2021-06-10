27d3e071ce5b80b5c5a6732d317a2c72e2d2a41a
Al3x <roat_alexandru@yahoo.com>
2021-06-10 20:07:35 +0300

    files

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
-const cell = document.querySelector('.directors__container');
-const visibil = document.querySelector('.cell__visibile');
-const invisibil = document.querySelector('.cell__invisible');
+const cell = document.querySelector('.directors__container')
 
 cell.addEventListener('click', e => {
     
     if(e.target.tagName === 'I') {
-        if(!visibil.classList.contains('disp__non')) {
-            const parinte = e.target.parentElement;
-            console.log(parinte);
-            let firstChild = parinte.children[0];
-            let secondChild = parinte.children[1];
-
-            firstChild.classList.add('disp__non');
-            secondChild.classList.remove('disp__non');
-        
-        
-            let clasFirst = firstChild.className.split(' ');
-            console.log(firstChild, secondChild, clasFirst[1]);
-            console.log('are')
-
-        }else{
-            visibil.classList.remove('disp__non');
-            invisibil.classList.add('disp__non');
-        }
+        console.log(e.target)
+        //sa ii aflu parinte la I
+        const parinte = e.target.parentElement
+        console.log(parinte)
+        //selectez amandoi child
+        const copii = parinte.children
+        console.log(copii)
+        //verific la primu child daca are clasa de visibil, daca o are ii pun none
+        Array.from(copii).forEach((temp) => {
+            
+            if(temp.tagName === 'DIV') {
+              console.log('elementul savlat in temp este', temp)  
+              //la al doi-lea child fac acelas lucru, doar ca opus
+              if(!temp.classList.contains('disp__non')) {
+                    temp.classList.add('disp__non')  
+              }else{
+                  temp.classList.remove('disp__non')
+              }
+            }
+        })
     }
 });
 
